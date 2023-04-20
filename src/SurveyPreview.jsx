d2c2b1e826e4dded8ad11a9a2bb23c6641bf31fd
NitinPokhriyal30 <nitinpokhriyal30@gmail.com>
2023-04-20 21:38:28 +0530

    fix: survey preview input foucs bug

--- src/SurveyPreview.jsx
+++ src/SurveyPreview.jsx
@@ -12,6 +12,60 @@ import {
 } from '@mui/material'
 import * as React from 'react'
 
+const formInputTypeMap = {
+  textField: (props) => (
+    <div>
+      <TextField {...props} label={undefined} placeholder="Type here..." />
+    </div>
+  ),
+  checkBox: (props) => (
+    <FormControlLabel
+      sx={{ display: 'block', marginBottom: 1 }}
+      control={<Checkbox name={props.name} value={props.label} />}
+      label={props.label}
+      {...props}
+    />
+  ),
+  radioButton: (props) => (
+    <FormControlLabel
+      sx={{ display: 'block', marginBottom: 1 }}
+      control={
+        <input style={{ marginInline: '.8rem' }} type="radio" {...props} value={props.label} />
+      }
+      label={props.label}
+      {...props}
+    />
+  ),
+  select: ({ label, ...props }) => {
+    let options = label
+      .split(',')
+      .filter(Boolean)
+      .map((x) => x.trim())
+
+    return (
+      <TextField
+        select
+        required
+        {...props}
+        label={undefined}
+        SelectProps={{
+          native: true,
+          defaultValue: 'heyo',
+        }}
+      >
+        <option hidden value="">
+          Choose one below:
+        </option>
+        {options.map((optionValue) => (
+          <option value={optionValue} key={optionValue}>
+            {optionValue}
+          </option>
+        ))}
+      </TextField>
+    )
+  },
+}
+
 export default function SurveyPreview({ style, form, onClose, ...props }) {
   const [formSubmission, setFormSubmission] = React.useState(() =>
     Array.from(form).map((input) => ({
@@ -21,60 +75,6 @@ export default function SurveyPreview({ style, form, onClose, ...props }) {
       required: input.required,
     }))
   )
-
-  const formInputTypeMap = {
-    textField: (props) => (
-      <div>
-        <TextField {...props} label={undefined} placeholder="Type here..." />
-      </div>
-    ),
-    checkBox: (props) => (
-      <FormControlLabel
-        sx={{ display: 'block', marginBottom: 1 }}
-        control={<Checkbox name={props.name} value={props.label} />}
-        label={props.label}
-        {...props}
-      />
-    ),
-    radioButton: (props) => (
-      <FormControlLabel
-        sx={{ display: 'block', marginBottom: 1 }}
-        control={
-          <input style={{ marginInline: '.8rem' }} type="radio" {...props} value={props.label} />
-        }
-        label={props.label}
-        {...props}
-      />
-    ),
-    select: ({ label, ...props }) => {
-      let options = label
-        .split(',')
-        .filter(Boolean)
-        .map((x) => x.trim())
-
-      return (
-        <TextField
-          select
-          required
-          {...props}
-          label={undefined}
-          SelectProps={{
-            native: true,
-            defaultValue: 'heyo',
-          }}
-        >
-          <option hidden value="">
-            Choose one below:
-          </option>
-          {options.map((optionValue) => (
-            <option value={optionValue} key={optionValue}>
-              {optionValue}
-            </option>
-          ))}
-        </TextField>
-      )
-    },
-  }
 
   return (
     <Box
@@ -98,45 +98,40 @@ export default function SurveyPreview({ style, form, onClose, ...props }) {
         </Button>
       </Box>
       {form.map((formInput, i) => (
-        <Box sx={{ marginBottom: 5 }}>
+        <Box key={formInput.id} sx={{ marginBottom: 5 }}>
           <Typography variant="h6" sx={{ marginBottom: 2 }}>
             {i + 1}. {formInput.question}
             {formInput.required && ' *'}
           </Typography>
 
-          {console.log(formSubmission)}
-
           {formInput.children.map((child, child_i) => {
             const InputControl = formInputTypeMap[child.type]
 
             return (
-              <>
-                <InputControl
-                  name={formInput.id}
-                  key={child_i}
-                  {...child.props}
-                  // required={formInput.required}
-                  value={formSubmission[i].value}
-                  checked={
-                    child.type === 'checkBox'
-                      ? formSubmission[i].value.includes(child.props.label)
-                      : formSubmission[i].value === child.props.label
-                  }
-                  onChange={(ev) => {
-                    setFormSubmission((prev) => {
-                      const input = ev.target
-                      if (input.type === 'checkbox') {
-                        if (input.checked) prev[i].value.push(input.value)
-                        else prev[i].value = prev[i].value.filter((value) => value !== input.value)
-                      } else {
-                        prev[i].value = input.value
-                      }
+              <InputControl
+                name={formInput.id}
+                key={child_i}
+                {...child.props}
+                value={formSubmission[i].value}
+                checked={
+                  child.type === 'checkBox'
+                    ? formSubmission[i].value.includes(child.props.label)
+                    : formSubmission[i].value === child.props.label
+                }
+                onChange={(ev) => {
+                  setFormSubmission((prev) => {
+                    const input = ev.target
+                    if (input.type === 'checkbox') {
+                      if (input.checked) prev[i].value.push(input.value)
+                      else prev[i].value = prev[i].value.filter((value) => value !== input.value)
+                    } else {
+                      prev[i].value = input.value
+                    }
 
-                      return [...prev]
-                    })
-                  }}
-                />
-              </>
+                    return [...prev]
+                  })
+                }}
+              />
             )
           })}
 
