836ee311cbf0ec1c8960d87209cd63f755ca2e3d
nitinpokhriyal <nitinpokhriyal30@gmail.com>
2023-05-05 14:48:07 +0530

    changes in talwind.config & mail file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5486E3",
+      },
+      fontFamily: {
+        Lato: ["Lato"],
+      },
+    },
   },
   plugins: [],
 }
